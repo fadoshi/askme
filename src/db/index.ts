41e9df6a82bff4b3c5b0a464932c2705c78106ff
fadoshi <doshiforama@gmail.com>
2025-07-17 09:56:03 -0500

    auth setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,13 @@
 import { drizzle } from 'drizzle-orm/neon-http';
+//import { neon } from "@neondatabase/serverless";
 
 export const db = drizzle(process.env.DATABASE_URL!);
+/* if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+// Create a Neon client using the DATABASE_URL
+const sql = neon(process.env.DATABASE_URL);
+
+// Pass the Neon client to Drizzle
+export const db = drizzle(sql); */
